67d11fd8a8300b9bca093ce873e282e0392f7aae
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-04-21 07:02:42 +0200

    V 1.1

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+
+// Firebase конфигурация
+const firebaseConfig = {
+  apiKey: "AIzaSyBzHEcrGfwek6FzguWbSGSfMgebMy1sBe8",
+  authDomain: "minibudget-4e474.firebaseapp.com",
+  projectId: "minibudget-4e474",
+  storageBucket: "minibudget-4e474.firebasestorage.app",
+  messagingSenderId: "306275735842",
+  appId: "1:306275735842:web:740615c23059e97cd36d7b"
+};
+
+// Инициализация Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
