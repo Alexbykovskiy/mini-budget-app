7e9b698648abff55794f5aad42fbfc76f1f292f1
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-04-29 07:43:14 +0200

    1

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,1 @@
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBzHEcrGfwek6FzguWbSGSfMgebMy1sBe8",
-  authDomain: "minibudget-4e474.firebaseapp.com",
-  projectId: "minibudget-4e474",
-  storageBucket: "minibudget-4e474.appspot.com",
-  messagingSenderId: "306275735842",
-  appId: "1:306275735842:web:740615c23059e97cd36d7b"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Подключение Chart.js
-const script = document.createElement("script");
-script.src = "https://cdn.jsdelivr.net/npm/chart.js";
-document.head.appendChild(script);
+// Firebase config placeholder
