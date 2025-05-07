0e5092c58ee65d50a1a5266df2f1c629bcf8d231
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-05-07 07:23:00 +0200

    1

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('mini-budget-cache-v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './app.js',
+        './firebase-config.js',
+        './mini-icon.png',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
