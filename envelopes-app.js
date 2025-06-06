c890ea0bf57af8d7a8ff18c78f8bae37ea125873
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-06-06 08:13:55 +0200

    Update envelopes-app.js

--- envelopes-app.js
+++ envelopes-app.js
@@ -283,11 +283,15 @@ async function openDistributionEditor() {
     row.style.marginBottom = "16px";
     const percentValue = data.percent || 0;
     row.innerHTML = `
-      <label style='display:block; font-weight:bold; margin-bottom:4px;'>
-        <span id='label-${doc.id}' style='margin-right:8px;'>${percentValue}%</span>${data.name}
-      </label>
-      <input type='range' min='0' max='100' step='1' value='${percentValue}' id='range-${doc.id}' style='width:100%'>
-    `;
+      row.innerHTML = `
+  <label style='display:block; font-weight:bold; margin-bottom:4px;'>${data.name}</label>
+  <div style='display: flex; align-items: center; gap: 8px; margin-top: 2px;'>
+    <span style='font-size: 0.75em; width: 28px; text-align: left;'>0%</span>
+    <input type='range' min='0' max='100' step='1' value='${data.percent || 0}' id='range-${doc.id}' style='flex: 1; margin: 0;'>
+    <span style='font-size: 0.75em; width: 32px; text-align: right;'>100%</span>
+  </div>
+  <span id='label-${doc.id}' style='font-size:0.8em;'>${data.percent || 0}%</span>
+`;
     container.appendChild(row);
     ranges.push({ id: doc.id });
   });
