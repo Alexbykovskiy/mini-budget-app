d5145426fc419ae0b4a6bf2a2285ed8250ecc655
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-06-16 07:34:07 +0200

    1

--- envelopes-app.js
+++ envelopes-app.js
@@ -1181,8 +1181,8 @@ function showEnvelopeMenu(btn, id) {
   // Вставить пилюлю меню слева от кнопки меню:
  btn.parentNode.insertBefore(menu, btn);
 
-menu.style.marginTop = '-5px';     // вверх
-menu.style.marginRight = '-9px';    // влево
+menu.style.marginTop = '-4.5px';     // вверх
+menu.style.marginRight = '-8.5px';    // влево
 
   // Прячем "Удалить" для спец-конвертов
   db.collection("envelopes").doc(id).get().then(doc => {
