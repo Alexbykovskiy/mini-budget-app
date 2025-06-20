1859802140c87e130318cb0c940bcf0b436d2cfd
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-06-20 17:20:01 +0200

    1

---
+++ tattoo-app.js
@@ -0,0 +1,22 @@
+// Инициализация Firebase
+// вставь сюда свой firebaseConfig
+
+function addIncome() {
+  alert('Добавление дохода будет реализовано позже.');
+}
+
+function addExpense() {
+  alert('Добавление расхода будет реализовано позже.');
+}
+
+function calculateStats() {
+  alert('Расчёт статистики будет реализован позже.');
+}
+
+function showCalendar() {
+  alert('Календарь будет реализован позже.');
+}
+
+function showSettings() {
+  alert('Настройки будут реализованы позже.');
+}
