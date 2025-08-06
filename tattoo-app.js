eb4ef4978840e21577cc5722ad27f55ab4a33dab
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-08-06 22:12:58 +0200

    Update tattoo-app.js

--- tattoo-app.js
+++ tattoo-app.js
@@ -17,7 +17,7 @@ const db = firebase.firestore();
 async function loadStudios() {
   studios = [];
   const snap = await db.collection('studios').get();
-  nap.forEach(doc => studios.push({ id: doc.id, ...doc.data(), studio: doc.data().studio }));
+  snap.forEach(doc => studios.push({ id: doc.id, ...doc.data(), studio: doc.data().studio }));
 
    renderStudioOptions();
   renderStudioSelect?.();
