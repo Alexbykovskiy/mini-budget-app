7a2fbef64c931f1afa60ccdcd87f4c5c76396a4f
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-12-15 11:22:23 +0100

    1

--- TattooCRM/app.js
+++ TattooCRM/app.js
@@ -1895,7 +1895,7 @@ function addSessionField(s = { dt: '', price: '', done: false }) {
            placeholder="€"
            value="${(s.price ?? '')}"
            title="Стоимость сеанса, €"
-           style="width:70px">
+           style="width:55px">
 
     <!-- 4) Удалить -->
     <button type="button" class="btn danger" title="Удалить дату">✕</button>
